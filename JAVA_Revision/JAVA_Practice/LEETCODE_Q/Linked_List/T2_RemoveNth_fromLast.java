9dbce3968838b1dccf561dd84c2d9689dae26c2b
Priyesh Rai <130801748+Priyesh-Rai-011@users.noreply.github.com>
2024-07-26 01:49:41 +0530

    18-07-2024 first commit

---
+++ JAVA_Revision/JAVA_Practice/LEETCODE_Q/Linked_List/T2_RemoveNth_fromLast.java
@@ -0,0 +1,31 @@
+package LEETCODE_Q.Linked_List;
+
+import java.util.LinkedList;
+import java.util.List;
+
+class ListNode { int val;ListNode next;ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+public class T2_RemoveNth_fromLast {
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummyHead = new ListNode(0,head);
+        ListNode fast = dummyHead, slow = dummyHead;
+        for (int i=0; i<=n; i++){
+            fast = fast.next;
+        }
+        while (fast.next!=null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+    public static void main(String[] args) {
+//        removeNthFromEnd(nums,4);
+    }
+}
+/*
+*
+*
+*/
