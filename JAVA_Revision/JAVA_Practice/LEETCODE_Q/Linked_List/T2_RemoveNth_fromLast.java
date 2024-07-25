54011aed5de0eb92b7b13a554ebdad622e5525d7
Priyesh Rai <130801748+Priyesh-Rai-011@users.noreply.github.com>
2024-07-26 01:51:33 +0530

    18-07-2024 first commit

--- JAVA_Revision/JAVA_Practice/LEETCODE_Q/Linked_List/T2_RemoveNth_fromLast.java
+++ JAVA_Revision/JAVA_Practice/LEETCODE_Q/Linked_List/T2_RemoveNth_fromLast.java
@@ -1,8 +1,5 @@
 package LEETCODE_Q.Linked_List;
 
-import java.util.LinkedList;
-import java.util.List;
-
 class ListNode { int val;ListNode next;ListNode() {}
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
