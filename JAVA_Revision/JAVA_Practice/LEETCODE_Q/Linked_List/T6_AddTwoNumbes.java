b9d34f0db3b0dab912be51f63f61ed7517e288a3
Priyesh Rai <130801748+Priyesh-Rai-011@users.noreply.github.com>
2024-07-28 19:41:08 +0530

    18-07-2024 first commit

---
+++ JAVA_Revision/JAVA_Practice/LEETCODE_Q/Linked_List/T6_AddTwoNumbes.java
@@ -0,0 +1,12 @@
+package LEETCODE_Q.Linked_List;
+
+public class T6_AddTwoNumbes {
+    class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+    public static void main(String[] args) {
+
+    }
+}
