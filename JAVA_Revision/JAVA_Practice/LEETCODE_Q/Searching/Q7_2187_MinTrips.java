4ee6b3b4a7a38c02e1c5e0c913d842e70c58fe24
Priyesh Rai <130801748+Priyesh-Rai-011@users.noreply.github.com>
2024-08-06 15:41:38 +0530

    05-08-2024 1st commit of today

--- JAVA_Revision/JAVA_Practice/LEETCODE_Q/Searching/Q7_2187_MinTrips.java
+++ JAVA_Revision/JAVA_Practice/LEETCODE_Q/Searching/Q7_2187_MinTrips.java
@@ -1,8 +1,25 @@
 package LEETCODE_Q.Searching;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Q7_2187_MinTrips {
 
+    public static long minimumTime(int[] time, int totalTrips) {
+        int[] trips = new int[time.length];
+        int tripsDone = 0, t = 0;
+        while(tripsDone < totalTrips){
+            t++;
+            for(int i=0; i<time.length; i++){
+                trips[i] = (trips[i]+1)%time[i];
+                if(trips[i]==0){
+                    tripsDone++;
+                }
+            }
+        }
+        return t;
+    }
     public static void main(String[] args) {
-
+        System.out.println(minimumTime(new int[]{1,2,3},5));
     }
 }
