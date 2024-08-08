5f425030ceaa4eca39a5b3a6dca6a24367356752
Priyesh Rai <130801748+Priyesh-Rai-011@users.noreply.github.com>
2024-08-09 01:57:45 +0530

    08-08-2024 3rd commit of today

--- JAVA_Revision/JAVA_Practice/LEETCODE_Q/String/Q13_ShiftingLetters.java
+++ JAVA_Revision/JAVA_Practice/LEETCODE_Q/String/Q13_ShiftingLetters.java
@@ -1,8 +1,17 @@
 package LEETCODE_Q.String;
 
 public class Q13_ShiftingLetters {
+    public static String shiftingLetters(String s,int[] shifts){
+        char[] result = s.toCharArray();
+        int t=0, n=s.length();
+        for (int i=n-1; i>=0; i--){
+            t+=shifts[i];
+            result[i]+=t;
+        }
+        return String.valueOf(result);
+    }
 
     public static void main(String[] args) {
-
+        System.out.println(shiftingLetters("priy",new int[]{1,2,3,4}));
     }
 }
