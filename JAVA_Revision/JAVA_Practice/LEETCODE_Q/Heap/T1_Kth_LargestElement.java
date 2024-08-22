9e0d369a38238912b552b90c524bd9b408a68686
Priyesh Rai <130801748+Priyesh-Rai-011@users.noreply.github.com>
2024-08-22 16:07:31 +0530

    22-08-2024 4th commit of today

--- JAVA_Revision/JAVA_Practice/LEETCODE_Q/Heap/T1_Kth_LargestElement.java
+++ JAVA_Revision/JAVA_Practice/LEETCODE_Q/Heap/T1_Kth_LargestElement.java
@@ -8,18 +8,34 @@ import java.util.PriorityQueue;
 * Approach 2: largest element --> max heap implementation (GENERAL)
 * */
 public class T1_Kth_LargestElement {
-    static int kth_largest_app1(int[] nums, int k){
-        PriorityQueue<Integer> hp = new PriorityQueue<>();
-        for(int i=0; i< nums.length; i++){
-            hp.add(nums[i]);
-            if (hp.size() > k){
-                hp.poll();
-            }
-        }
-        return hp.peek();
+//                        =====  Approach 1 =====
+//    static int kth_largest_app1(int[] nums, int k){
+//        PriorityQueue<Integer> hp = new PriorityQueue<>();
+//        for(int i=0; i< nums.length; i++){
+//            hp.add(nums[i]);
+//            if (hp.size() > k){
+//                hp.poll();
+//            }
+//        }
+//        return hp.peek();
+//    }
+//                        =====  Approach 2 =====
+static int kth_largest_app2(int[] arr, int k) {
+    PriorityQueue<Integer>pq= new PriorityQueue<>((a,b)->b-a);
+    for (int i = 0; i < arr.length; i++) {
+        pq.add(arr[i])  ;
     }
+    int f = k - 1 ;
+    for(int i=0; i<f; i++){
+        pq.remove()  ;
+    }
+
+    System.out.println(k+"th Largest element "+pq.peek())  ;
+    return pq.peek();
+}
     public static void main(String[] args) {
         int[] nums = { 11, 3, 2, 1, 15, 5, 4, 45, 88, 96, 50, 45 };
-        System.out.println(kth_largest_app1(nums,3));
+//        System.out.println(kth_largest_app1(nums,3));
+        System.out.println(kth_largest_app2(nums,3));
     }
 }
