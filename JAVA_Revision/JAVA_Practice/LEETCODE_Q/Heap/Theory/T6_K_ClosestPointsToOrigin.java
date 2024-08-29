cce028c88c3bf097e3fc1a61a8787a93ec8c0114
Priyesh Rai <130801748+Priyesh-Rai-011@users.noreply.github.com>
2024-08-29 18:42:57 +0530

    29-08-2024 1st commit of today

--- JAVA_Revision/JAVA_Practice/LEETCODE_Q/Heap/Theory/T6_K_ClosestPointsToOrigin.java
+++ JAVA_Revision/JAVA_Practice/LEETCODE_Q/Heap/Theory/T6_K_ClosestPointsToOrigin.java
@@ -1,4 +1,24 @@
 package LEETCODE_Q.Heap.Theory;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 public class T6_K_ClosestPointsToOrigin {
+    public static int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> ((a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1])));
+        for (int[] point : points){
+            heap.offer(point);
+        }
+//        System.out.println(heap);
+        int[][] result = new int[k][2];
+        for (int i=0; i<k; i++){
+            result[i] = heap.poll();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.deepToString(kClosest(new int[][]{{3, 3}, {5, -1}, {-2, 4}}, 2)));
+    }
 }
