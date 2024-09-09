e31b8db15e4cf55ce2f612262d1ad2beb47475b1
Priyesh Rai <130801748+Priyesh-Rai-011@users.noreply.github.com>
2024-09-09 13:09:48 +0530

    31-08-2024 1st commit of today

--- JAVA_Revision/JAVA_Practice/LEETCODE_Q/Recursion/theory_AV/T1_MaxDepth_BT.java
+++ JAVA_Revision/JAVA_Practice/LEETCODE_Q/Recursion/theory_AV/T1_MaxDepth_BT.java
@@ -1,10 +1,6 @@
 package LEETCODE_Q.Recursion.theory_AV;
-
-import javax.management.MBeanAttributeInfo;
-
-class T1_MaxDepth_BT {
-    public class TreeNode {
-      int val; TreeNode left; TreeNode right;
+class TreeNode {
+      int val;TreeNode left;TreeNode right;
       TreeNode() {}
       TreeNode(int val) { this.val = val; }
       TreeNode(int val, TreeNode left, TreeNode right) {
@@ -13,12 +9,16 @@ class T1_MaxDepth_BT {
           this.right = right;
       }
   }
-    class Solution {
-        public int maxDepth(TreeNode root) {
-            return 0;
-        }
+public class T1_MaxDepth_BT {
+    public static int maxDepth(TreeNode root){
+        if (root==null){return 0;}
+        int l = maxDepth(root.left);
+        int r= maxDepth(root.right);
+
+        return Math.max(l,r)+1;
     }
     public static void main(String[] args) {
-        System.out.println("Max depth of binary tree : ");
+        System.out.println("Finding the max depth of a Binary Tree");
+
     }
 }
