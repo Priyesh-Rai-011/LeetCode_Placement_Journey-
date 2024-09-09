2df9cd3c65084b4f8caff7d8cbca724cbf1c57b0
Priyesh Rai <130801748+Priyesh-Rai-011@users.noreply.github.com>
2024-09-09 17:16:10 +0530

    09-09-2024 1st commit of today

---
+++ JAVA_Revision/JAVA_Practice/LEETCODE_Q/Recursion/theory_AV/T3_SortStack_recursion.java
@@ -0,0 +1,38 @@
+package LEETCODE_Q.Recursion.theory_AV;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+public class T3_SortStack_recursion {
+    public static void sortStack(Deque<Integer> stk){
+        if(!stk.isEmpty()){
+            int x = stk.pollFirst();
+            sortStack(stk);
+            sortedInsert(stk,x);
+        }
+    }
+    public static void sortedInsert(Deque<Integer> stk, int x){
+        if (stk.isEmpty() || x>stk.peekFirst()){
+            stk.offerFirst(x);
+            return;
+        }
+//        else {
+            int temp = stk.pollFirst();
+            sortedInsert(stk,x);
+            stk.offerFirst(temp);
+//        }
+    }
+    public static void main(String[] args) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.offerFirst(30);
+        deque.offerFirst(-5);
+        deque.offerFirst(18);
+        deque.offerFirst(14);
+        deque.offerFirst(-3);
+
+        System.out.println(deque);
+        sortStack(deque);
+        System.out.println(deque);
+    }
+}
---
+++ JAVA_Revision/JAVA_Practice/LEETCODE_Q/Recursion/theory_AV/T4_ReverseStack_Recursion.java
@@ -0,0 +1,41 @@
+package LEETCODE_Q.Recursion.theory_AV;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class T4_ReverseStack_Recursion {
+    public static void reverse(Deque<Integer> stk){
+        if(stk.size()>0){
+            int x = stk.peekFirst();
+            stk.pollFirst();
+            reverse(stk);
+            insertAtLast(stk,x);
+        }
+    }
+    public static void insertAtLast(Deque<Integer>stk, int x){
+        if (stk.isEmpty()){
+            stk.offerFirst(x);
+        }
+        else {
+            int a = stk.peekFirst();
+            stk.pollFirst();
+            insertAtLast(stk, x);
+            stk.offerFirst(a);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Reversing a stack using recursion : ");
+        Deque<Integer> stk = new ArrayDeque<>();
+        stk.offerFirst(1);
+        stk.offerFirst(3);
+        stk.offerFirst(4);
+        stk.offerFirst(5);
+        stk.offerFirst(7);
+        stk.offerFirst(9);
+        stk.offerFirst(11);
+
+        System.out.println(stk);
+        reverse(stk);
+        System.out.println(stk);
+    }
+}
